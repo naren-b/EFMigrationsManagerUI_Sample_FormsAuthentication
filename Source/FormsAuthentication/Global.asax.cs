9437501ad8473b108ae650268b02147aad0ad433
naren-b <34547623+naren-b@users.noreply.github.com>
2017-12-15 13:39:57 -0600

    Initial commit

---
+++ Source/FormsAuthentication/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FormsAuthentication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+        }
+
+        public void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+
+            //Alternative approach(from global filter VerifyIsEFMigrationUpToDateAttribute) to redirect to Database deployment page
+            //if (EFMigrationsManagerConfig.HandleEFMigrationException(exception, Server, Response, Context))
+            //    return;
+        }
+    }
+}
